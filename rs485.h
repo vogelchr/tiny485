ffc304f40573b99dd8edb48e166c30d3f8f1d536
Christian Vogel <vogelchr@vogel.cx>
2017-07-13 20:29:06 +0200

    Work in progress.

--- rs485.h
+++ rs485.h
@@ -12,7 +12,7 @@ extern unsigned char rs485_txbuf[RS485_MAX_MSGSIZE];
 /* poll returns NOMSG when there is no new message */
 #define RS485_POLL_NOMSG 0xff
 extern unsigned char rs485_poll();
-
+extern void rs485_rxok();
 
 extern void rs485_start_tx(unsigned char len);
 
